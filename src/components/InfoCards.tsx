8f4b1d7c26382ce60a806ebd8709c8d500a951e1
Shan15Dev <shansai@bluewin.ch>
2022-11-17 15:17:26 +0100

    InfoCard was created

--- src/components/InfoCards.tsx
+++ src/components/InfoCards.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { InfoCardsProps } from '../types/Custom/InfoCardType';
 
-function InfoCards() {
+
+
+function InfoCards(infoCardProps : InfoCardsProps[]) {
+    const [infoProps, setInfoProps] = useState<InfoCardsProps[]>(infoCardProps)
     return (
         <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5 place-items-center mt-5">
             {
-                repositories?.map(repo => (
+                infoProps.map(info => (
                     <div
                         className="card w-96 bg-primary text-primary-content cursor-pointer md:transform md:transition md:duration-500 md:hover:scale-125"
-                        key={repo.id}>
-                        <a href={repo.html_url}>
+                        key={info.title}>
+                        <a href={info.link}>
                             <div className="card-body">
-                                <h2 className="card-title">{repo.name}</h2>
-                                <p>{repo.description ? repo.description : "No description"}</p>
+                                <h2 className="card-title">{info.title}</h2>
                             </div>
                         </a>
                     </div>
---
+++ src/types/Custom/InfoCardType.ts
@@ -0,0 +1,6 @@
+type InfoCardsProps = {
+    title: string,
+    link: string
+}
+
+export type  {InfoCardsProps}
