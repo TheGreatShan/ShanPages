cf81741bc1a60e39a01220ac04290d53e3a3efd6
Shan15Dev <shansai@bluewin.ch>
2022-11-17 16:32:50 +0100

    Infocard was implemented to Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import InfoCards from '../components/InfoCards';
+import {InfoCardsProps} from "../types/Custom/InfoCardType";
+import Profile from "../components/Profile";
 
 function Home() {
+    document.title = "Home - Muras"
+
+    const [infoCardList, setInfoCardList] = useState<InfoCardsProps[]>([{
+        title: "Projects",
+        link: "/projects"
+    }, {title: " About", link: "/about"}, {title : "BBW Winterthur TimeTable", link: "/timetable"}])
+
     return (
-        <div>
-            
+        <div className="bg-base-100">
+            <Profile profileName="shan15dev"/>
+            <InfoCards infoCardProps={infoCardList}/>
         </div>
     );
 }
