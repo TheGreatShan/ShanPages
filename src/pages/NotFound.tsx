59aceb15644c718a193096d937302aef2060c974
Shan15Dev <61542805+Shan15Dev@users.noreply.github.com>
2022-12-31 19:39:00 +0100

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,7 +7,7 @@ function NotFound() {
     return (
         <div className={"flex flex-col justify-center items-center"}>
             <h1 className={"mt-10 text-center text-3xl font-bold text-slate-100"}>404 - Not Found</h1>
-            <p className={"mt-10"}>The page you searched has not been found. Please either return to the home page.</p>
+            <p className={"mt-10"}>The page you searched has not been found. Please return to the home page.</p>
             <input className={"mt-10 bg-primary rounded p-3 text-slate-800"} type="button" value="Return to Home" onClick={() => navigate("/")}/>
         </div>
     );
