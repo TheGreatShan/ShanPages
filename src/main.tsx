ad439e723f3f74f7edc031004fb01b8ed8a59b49
shan <shansai@bluewin.ch>
2023-05-27 19:19:41 +0200

    Browserrouter has been moved to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import Navbar from "./components/Navbar";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <Navbar/>
-        <App/>
+        <BrowserRouter>
+            <Navbar/>
+            <App/>
+        </BrowserRouter>
     </React.StrictMode>
 )
