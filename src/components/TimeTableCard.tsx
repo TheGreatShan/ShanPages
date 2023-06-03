29389f03bde5ce7a4d0f92a2ba0679fd31f31896
shan <shansai@bluewin.ch>
2023-06-03 09:56:26 +0200

    :lipstick: Timetable card design has been modified and nanopseconds are shown in time

--- src/components/TimeTableCard.tsx
+++ src/components/TimeTableCard.tsx
@@ -3,6 +3,7 @@ import {InfoCardsProps} from '../types/Custom/InfoCardType';
 import {Link} from "react-router-dom";
 import {Subject} from "../types/Custom/SubjectType";
 import {getTimeTable} from "../lib/api";
+import {it} from "node:test";
 
 type Property = {
     day: string
@@ -26,22 +27,34 @@ function InfoCards({day}: Property) {
     }, [])
     console.log(infoProps)
     return (
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-5 place-items-center mt-5 bg-base-100">
+        <div className="grid gap-5 place-items-center mt-5 bg-base-100">
             {
                 infoProps.map(info => (
                     <div
-                        className="card w-96 bg-primary text-primary-content cursor-pointer md:transform md:transition md:duration-500 md:hover:scale-125"
+                        className="card card-compact w-96 bg-primary text-primary-content cursor-pointer md:transform md:transition md:duration-500 md:hover:scale-105"
                         key={info.subject}>
                         <div className="card-body">
                             <h2 className="card-title">{info.subject}</h2>
-                            <p>{info.start.seconds}</p>
-                            <p>{info.end.seconds}</p>
+                            <div className="grid grid-cols-2">
+                                <p><span className="font-bold">Start: </span>{nanosecondsToDate(info.start.seconds * 1000000000 + info.start.nanoseconds).toLocaleTimeString()}</p>
+                                <p><span className="font-bold">End: </span>{nanosecondsToDate(info.end.seconds * 1000000000 + info.end.nanoseconds).toLocaleTimeString()}</p>
+                                <p><span className="font-bold">Place: </span>{info.room}</p>
+                            </div>
                         </div>
                     </div>
                 ))
             }
         </div>
     );
+}
+
+function nanosecondsToDate(nanoseconds: number): Date {
+    const seconds = Math.floor(nanoseconds / 1000000000);
+    const milliseconds = nanoseconds / 1000000 - seconds * 1000;
+    const date = new Date(0);
+    date.setUTCSeconds(seconds);
+    date.setUTCMilliseconds(milliseconds);
+    return date;
 }
 
 export default InfoCards;
