3919cef151c8ad0bbe404438ff0a6dab0958c811
shan15dev <shansai@bluewin.ch>
2023-06-24 21:47:17 +0200

    :lipstick: Header is bigger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,10 +6,10 @@ type HeaderProps = {
 
 function Header({title}: HeaderProps) {
     return (
-        <div>
-            <header className="shadow bg-base-100">
+        <div className={"mb-3"}>
+            <header className="bg-base-100">
                 <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    <h1 className="text-center text-3xl font-bold text-slate-100">{title}</h1>
+                    <h1 className="text-center text-6xl font-bold text-slate-100">{title}</h1>
                 </div>
             </header>
         </div>
