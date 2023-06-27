36cb247fa2449d0b91ece79a79443842795abc81
shan <shansai@muraleetharan.ch>
2023-06-27 15:46:01 +0200

    :lipstick: timetablecard is adjusted to mobile version

--- src/components/TimeTableCard.tsx
+++ src/components/TimeTableCard.tsx
@@ -16,11 +16,11 @@ function InfoCards({day}: Property) {
             {
                 day.map(info => (
                     <div
-                        className="card cursor-default card-compact w-1/4 bg-primary text-primary-content cursor-pointer md:transform md:transition md:duration-500 md:hover:scale-105"
+                        className="card cursor-default card-compact md:w-1/4 w-full bg-primary text-primary-content md:transform md:transition md:duration-500 md:hover:scale-105"
                         key={info.subject}>
                         <div className="card-body">
                             <h2 className="card-title">{info.subject}</h2>
-                            <div className="grid grid-cols-1 md:grid-cols-3">
+                            <div className="grid grid-cols-2 md:grid-cols-3">
                                 <p><span
                                     className="font-bold">Start: </span>{nanosecondsToDate(info.start.seconds * 1000000000 + info.start.nanoseconds).toLocaleTimeString([], {
                                     hour: "2-digit",
