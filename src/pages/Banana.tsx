ff999849d13639a1a008eea90c8c11118f644277
Shansai Muraleetharan <shansai@muraleetharan.ch>
2024-03-28 07:41:57 +0100

    Banana page

---
+++ src/pages/Banana.tsx
@@ -0,0 +1,11 @@
+export default function Banana() {
+  return (
+    <div>
+      <h1>Banana</h1>
+      <img
+        src="https://th.bing.com/th/id/OIP.wa91Wb1CWt1mwKAK0qYLHQHaE9?rs=1&pid=ImgDetMain"
+        alt="picture of a banana"
+      />
+    </div>
+  );
+}
